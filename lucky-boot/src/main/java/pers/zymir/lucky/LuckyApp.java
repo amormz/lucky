d0ff6857eab3fec8f7a9f8ae26af4a3383c66434
李子木 <zimu.li@renren-inc.com>
2023-07-17 17:17:56 +0800

    application & banner

---
+++ lucky-boot/src/main/java/pers/zymir/lucky/LuckyApp.java
@@ -0,0 +1,11 @@
+package pers.zymir.lucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyApp {
+  public static void main(String[] args) {
+    SpringApplication.run(LuckyApp.class, args);
+  }
+}
