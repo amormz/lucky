ac0f38b7be05de709fee0c366046d708aa4edb68
李子木 <zimu.li@renren-inc.com>
2023-08-10 18:44:22 +0800

    开启事务

--- lucky-boot/src/main/java/pers/zymir/lucky/LuckyApp.java
+++ lucky-boot/src/main/java/pers/zymir/lucky/LuckyApp.java
@@ -3,9 +3,11 @@ package pers.zymir.lucky;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("pers.zymir.lucky.dao")
+@EnableTransactionManagement
 public class LuckyApp {
     public static void main(String[] args) {
         SpringApplication.run(LuckyApp.class, args);
